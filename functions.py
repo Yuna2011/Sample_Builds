bb789c07503598ada38a08faf13d5a75f41f1979
Yuna2011 <yuna.shin1@education.nsw.gov.au>
2026-03-02 09:49:41 +1100

    headings
    Changed all the names of the functions and lists to match my idea from the pokedex and pokemon.

--- functions.py
+++ functions.py
@@ -2,9 +2,12 @@ import requests # Must import requests so we can use API
  
 
 # API Base URL for PokéAPI
-API_URL = "https://dog.ceo/dog-api/documentation/"
+API_URL = "https://dog.ceo/api/breeds/list/all"
 
-def search_animal(name):
+# Dictionary to store collected Pokémon
+dog_list = {}
+
+def search_dog(name):
     """Search for an animal by name or ID and return its details.""" # Triple quotes is a docstring - allows multiline comments!
     response = requests.get(f"{API_URL}{name.lower()}")
     if response.status_code == 200:
@@ -14,11 +17,33 @@ def search_animal(name):
             "class": data["class"],
             "height": data["height"],
             "weight": data["weight"],
-#            "types": [t["type"]["name"] for t in data["types"]]
         }
     else:
-        print("animal not found.")
+        print("dog breed not found.")
         return None
+    
+def add_dog(name):
+    """Add a dog breed to the list."""
+    dog = search_dog(name)
+    if dog:
+        dog_list[dog["name"]] = dog
+        print(f"{dog['name']} added to list.")
+
+def view_dog_list():
+    """Display all collected Pokémon."""
+    if dog_list:
+        for name, details in dog_list.items():
+            print(f"{name} - ID: {details['id']}, Height: {details['height']}, Weight: {details['weight']}")
+    else:
+        print("Your dog breed list is empty.")
+
+def remove_dog(name):
+    """Remove a Pokémon from the Pokédex."""
+    if name.capitalize() in dog_list:
+        del dog_list[name.capitalize()]
+        print(f"{name.capitalize()} removed from list.")
+    else:
+        print("Dog breed not found in your list.")
 
 
 #pip install -r requirements.txt
--- main1.py
+++ main1.py
@@ -2,30 +2,30 @@ from functions import *
 
 def main():
     while True:
-        print("Animal Finder menu:")
-        print("1. Search Animal")
-#        print("2. Add Pokémon to Pokédex")
-#        print("3. View Pokédex")
-#        print("4. Remove Pokémon")
-        print("2. Exit")
+        print("Dog breed finder menu:")
+        print("1. Search dog breed")
+        print("2. Add dog breed to my list")
+        print("3. View my list")
+        print("4. Remove dog breed from my list")
+        print("5. Exit")
         choice = input("Choose an option: ")
 
         if choice == "1":
             name = input("Enter Animal name: ")
-            search_animal(name)
-            for key, value in search_animal(name).items(): 
+            search_dog(name)
+            for key, value in search_dog(name).items(): 
               print(f"{key}: {value}")
-#        elif choice == "2":
-#            name = input("Enter Pokémon name to add: ")
-#            add_pokemon(name)
-#        elif choice == "3":
-#            view_pokedex()
-#        elif choice == "4":
-#            name = input("Enter Pokémon name to remove: ")
-#            remove_pokemon(name)
-#        elif choice == "5":
-#            print("Exiting Pokédex.")
-#            break
+        elif choice == "2":
+            name = input("Enter dog breed name to add: ")
+            add_dog(name)
+        elif choice == "3":
+            view_dog_list()
+        elif choice == "4":
+            name = input("Enter dog breed name to remove: ")
+            remove_dog(name)
+        elif choice == "5":
+            print("Exiting list.")
+            break
         else:
             print("Invalid choice. Please try again.")
 
