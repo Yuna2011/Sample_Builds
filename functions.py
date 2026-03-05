c218dcd4d3efa511576ef2442fae217fef629643
Yuna2011 <yuna.shin1@education.nsw.gov.au>
2026-03-05 15:07:32 +1100

    feat : Got a working API and changed the quote
    I finally found a working API and changed the code from the Pokedex to fit the Harry Potter theme. One of the code needs fixing and I need to work on the markdowns. Other than that, I'm nearly done. Heh

--- functions.py
+++ functions.py
@@ -2,48 +2,49 @@ import requests # Must import requests so we can use API
  
 
 # API Base URL for PokéAPI
-API_URL = "https://dog.ceo/api/breeds/list/all"
+API_URL = "https://hp-api.onrender.com/api/characters"
 
 # Dictionary to store collected Pokémon
-dog_list = {}
+HP_list = {}
 
-def search_dog(name):
-    """Search for an animal by name or ID and return its details.""" # Triple quotes is a docstring - allows multiline comments!
+def search_character(name):
+    """Search for character by name and return its details.""" # Triple quotes is a docstring - allows multiline comments!
     response = requests.get(f"{API_URL}{name.lower()}")
     if response.status_code == 200:
         data = response.json()
         return {
             "name": data["name"].capitalize(),
-            "class": data["class"],
-            "height": data["height"],
-            "weight": data["weight"],
+            "alternate_names": data["alternate_names"],
+            "gender": data["gender"],
+            "house": data["house"],
+            "wand" : data["wand"]
         }
     else:
-        print("dog breed not found.")
+        print("Character not found.")
         return None
     
-def add_dog(name):
-    """Add a dog breed to the list."""
-    dog = search_dog(name)
-    if dog:
-        dog_list[dog["name"]] = dog
-        print(f"{dog['name']} added to list.")
+def add_character(name):
+    """Add a character to the list."""
+    hp = search_character(name)
+    if hp:
+        HP_list[hp["name"]] = hp
+        print(f"{hp['name']} added to list.")
 
-def view_dog_list():
-    """Display all collected Pokémon."""
-    if dog_list:
-        for name, details in dog_list.items():
+def view_character_list():
+    """Display all collected characters."""
+    if HP_list:
+        for name, details in HP_list.items():
             print(f"{name} - ID: {details['id']}, Height: {details['height']}, Weight: {details['weight']}")
     else:
-        print("Your dog breed list is empty.")
+        print("Your list is empty.")
 
-def remove_dog(name):
+def remove_character(name):
     """Remove a Pokémon from the Pokédex."""
-    if name.capitalize() in dog_list:
-        del dog_list[name.capitalize()]
+    if name.capitalize() in HP_list:
+        del HP_list[name.capitalize()]
         print(f"{name.capitalize()} removed from list.")
     else:
-        print("Dog breed not found in your list.")
+        print("Character not found in your list.")
 
 
 #pip install -r requirements.txt
--- main1.py
+++ main1.py
@@ -2,27 +2,26 @@ from functions import *
 
 def main():
     while True:
-        print("Dog breed finder menu:")
-        print("1. Search dog breed")
-        print("2. Add dog breed to my list")
+        print("Harry Potter character finder menu:")
+        print("1. Search character")
+        print("2. Add charater to my list")
         print("3. View my list")
-        print("4. Remove dog breed from my list")
+        print("4. Remove character from my list")
         print("5. Exit")
         choice = input("Choose an option: ")
 
         if choice == "1":
-            name = input("Enter Animal name: ")
-            search_dog(name)
-            for key, value in search_dog(name).items(): 
+            name = input("Enter character name: ")
+            for key, value in search_character(name).items(): 
               print(f"{key}: {value}")
         elif choice == "2":
-            name = input("Enter dog breed name to add: ")
-            add_dog(name)
+            name = input("Enter character to add: ")
+            add_character(name)
         elif choice == "3":
-            view_dog_list()
+            view_character_list()
         elif choice == "4":
-            name = input("Enter dog breed name to remove: ")
-            remove_dog(name)
+            name = input("Enter character to remove: ")
+            remove_character(name)
         elif choice == "5":
             print("Exiting list.")
             break
